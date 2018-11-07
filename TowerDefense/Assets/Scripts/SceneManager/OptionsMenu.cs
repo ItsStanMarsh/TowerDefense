edd75d3d118bb2b0bf59e844b84ff592db5d9b47
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2018-11-07 10:56:54 +0100

    menu stuff

---
+++ TowerDefense/Assets/Scripts/SceneManager/OptionsMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+	void Awake () {
+        DontDestroyOnLoad(this.gameObject);
+	}
+
+    void OnLevelWasLoaded(int level)
+    {
+        onOff = PlayerPrefs.GetInt("Music") == 1;
+    }
+
+    bool onOff;
+
+	public void Music () {
+
+        PlayerPrefs.SetInt("Music", onOff ? 0 : 1);
+        PlayerPrefs.Save();
+
+        onOff = !onOff;
+        AudioListener.volume = onOff ? 0 : 1;
+        
+	}
+}
---
+++ TowerDefense/Assets/Scripts/SceneManager/SceneSwitch/SceneSwitcher.cs
@@ -0,0 +1,49 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+//public class SceneSwitcher : MonoBehaviour {
+
+//	public void Switch (string sceneName) {
+
+ //       SceneManager.LoadScene(sceneName);
+
+//    }
+
+//}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    string prScene;
+    GameObject previousScene;
+
+    private void Start()
+    {
+        previousScene = GameObject.FindGameObjectWithTag("PrScene");
+        prScene = previousScene.GetComponent<PreviousScene>().previousScene;
+    }
+
+    public void startGame(string sceneName)
+    {
+        previousScene.GetComponent<PreviousScene>().SaveScene();
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void nextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void PreviousScene()
+    {
+        previousScene.GetComponent<PreviousScene>().SaveScene();
+        SceneManager.LoadScene(prScene);
+    }
+
+}
