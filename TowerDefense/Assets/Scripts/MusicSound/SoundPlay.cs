92cb378551a8023a116801226f4960c61fa74d70
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2018-11-15 09:14:57 +0100

    menu fixes

---
+++ TowerDefense/Assets/Scripts/MusicSound/SoundPlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundPlay : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        onOff = PlayerPrefs.GetInt("Sounds") == 1;
+    }
+
+    bool onOff;
+
+    public void Sounds()
+    {
+
+        PlayerPrefs.SetInt("Sounds", onOff ? 0 : 1);
+        PlayerPrefs.Save();
+
+        onOff = !onOff;
+        AudioListener.volume = onOff ? 0 : 1;
+
+    }
+}
---
+++ TowerDefense/Assets/Scripts/SceneManager/SceneSwitch/SimpleSwitch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleSwitch : MonoBehaviour {
+
+public void EasySwitch(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
